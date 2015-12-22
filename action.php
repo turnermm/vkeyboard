5a8f039ae9a37bffa5d7df0fa321f4fa1b96afd3
Myron Turner <turnermm02@shaw.ca>
2015-12-21 19:40:07 -0600

    declaration of action::register php7 compatible

--- action.php
+++ action.php
@@ -14,7 +14,7 @@ class action_plugin_vkeyboard extends DokuWiki_Action_Plugin {
     /**
      * Register its handlers with the DokuWiki's event controller
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
 
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'setVKIcookie');
 
